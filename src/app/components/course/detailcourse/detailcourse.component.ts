b0ee1254f1db34f00a18f390b28d04257396757b
6thom6 <66666050+6thom6@users.noreply.github.com>
2020-10-13 16:15:34 +0100

    13-10v2

--- src/app/components/course/detailcourse/detailcourse.component.ts
+++ src/app/components/course/detailcourse/detailcourse.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from 'src/app/models/course.model';
+import { dropdownlistItem } from 'src/app/models/dropdownlistItem.model';
+import { ChevalService } from 'src/app/service/cheval.service';
 import {CourseService} from 'src/app/service/course.service';
 
 @Component({
@@ -10,12 +12,37 @@ import {CourseService} from 'src/app/service/course.service';
 export class DetailcourseComponent implements OnInit {
 
   public courses : Course[] = [];
+  private _chevaux : dropdownlistItem [] = [];
+
+  public get chevaux(): dropdownlistItem [] {return this._chevaux}
 
   settings = {
     columns: {
 
       date_Courses: {
         title: 'date de Courses',
+      },
+      id_Courses : {
+        title : 'id course',
+      },
+      nom_Cheval : {
+        title : 'cheval',
+        type: 'html',
+        editor : {
+          type : 'list',
+          config : {
+            list : this.chevaux
+          }
+        }
+      },
+      race: {
+        title: 'race'
+      },
+      age: {
+        title: 'age'
+      },      
+      sexe: {
+        title: 'sexe'
       },
       distance: {
         title: 'distance'
@@ -45,11 +72,17 @@ export class DetailcourseComponent implements OnInit {
     }
   }
 
-  constructor(private _service : CourseService) { }
+  constructor(private _service : CourseService, private _chevalService : ChevalService) { }
 
   ngOnInit(): void
   {
-    this._service.get().subscribe(data => this.courses = data)       
+    this._service.get().subscribe(data => this.courses = data)   
+
+    this._chevalService.get().subscribe( data =>{ 
+      this._chevaux = data.map(function(d) {return {value: d.id_Cheval,title : d.nom_Cheval};});
+    this.settings.columns.nom_Cheval.editor.config.list=this.chevaux;
+    this.settings = Object.assign({}, this.settings);
+    });
 
   }
 
